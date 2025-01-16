15ccf379800da5a70ef9fb47073fa04fe9510840
Andy Drafahl <118315981+ACDrafahl@users.noreply.github.com>
2025-01-16 10:52:05 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Java2DTemplate
+
+This is a simple template I made for making different 2D Java projects using Swing. It's based on a gutted and restructured version of this repository: https://github.com/learncodebygaming/java_2d_game
---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testSomething() {
+        // Example test: test if something is true
+        assertEquals(1 + 1, 2);
+    }
+}
